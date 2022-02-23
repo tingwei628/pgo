419e62caab64f537d4e9f6f53742acbba33f0166
tingwei628 <sbvq6p199xe@gmail.com>
2022-02-23 16:02:27 +0800

    add 'say' command

--- remote_command/README.md
+++ remote_command/README.md
@@ -1,13 +1,22 @@
-## Feature
-- speak out the system information in cross platform \
+Speak out the system information in macOS \
 e.g. Current UTC time, CPU usage, Available RAM
-
-- espeak
 
 ## Usage
 
 ```
 go run main.go
+```
+
+**main.go**
+```go
+package main
+import (
+  "github.com/tingwei628/pgo/remote_command"
+)
+func main () {
+    rc := remote_command.RC{}
+    rc.Command()
+}
 ```
 
 ## Reference
--- remote_command/remote_command.go
+++ remote_command/remote_command.go
@@ -2,6 +2,8 @@ package remote_command
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
@@ -20,10 +22,10 @@ func init() {
 func (rc *RC) Command() {
 	memInfo, err := mem.VirtualMemory()
 	if err != nil {
-		fmt.Printf("get total mem info failed, err:%v", err)
+		log.Fatalf("get total mem info failed, err:%v", err)
 	}
 	// bytes to gigabytes
-	memAvailableStr := fmt.Sprintf("Available RAM %.3f GB\n", float64(memInfo.Available)/(1<<30))
+	memAvailableStr := fmt.Sprintf("Available RAM %.3f GB \n", float64(memInfo.Available)/(1<<30))
 
 	// cpuInfos, err := cpu.Info()
 	// if err != nil {
@@ -36,7 +38,7 @@ func (rc *RC) Command() {
 
 	// Current UTC time
 	// "2006-01-02 15:04:05" (go的誕生時間)
-	timeStr := fmt.Sprintf("Current UTC time %v\n", time.Now().UTC().Format("2006/01/02 15:04:05"))
+	timeStr := fmt.Sprintf("Current UTC time %v \n", time.Now().UTC().Format("2006/01/02 15:04:05"))
 	// 2022/02/23 06:37:01
 	//fmt.Printf("Current UTC time %v\n", time.Now().UTC().Format(time.UnixDate))
 	// Wed Feb 23 06:39:28 UTC 2022
@@ -44,13 +46,25 @@ func (rc *RC) Command() {
 	// CPU usage
 	percent, err := cpu.Percent(time.Second, false)
 	if err != nil {
-		fmt.Printf("get total cpu percent failed, err:%v", err)
+		log.Fatalf("get total cpu percent failed, err:%v", err)
 	}
-	percentStr := fmt.Sprintf("cpu total percent is %.2f%%\n", percent[0])
+	percentStr := fmt.Sprintf("cpu total percent is %.2f%% \n", percent[0])
 	// CPU usage over last hour
 
-	fmt.Print(timeStr)
-	fmt.Print(percentStr)
-	fmt.Print(memAvailableStr)
+	//fmt.Print(timeStr)
+	//fmt.Print(percentStr)
+	//fmt.Print(memAvailableStr)
 
+	// var strBuilder strings.Builder
+	// strBuilder.WriteString(timeStr)
+	// strBuilder.WriteString(percentStr)
+	// strBuilder.WriteString(memAvailableStr)
+	// fmt.Print(strBuilder.String())
+
+	for _, sentence := range []string{timeStr, percentStr, memAvailableStr} {
+		cmd := exec.Command("say", "-v", "fred", sentence)
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
